572d592f1d6a4644a77e1d2acf1b1b17232105b6
DESKTOP-HB0C1U8\Usuario <gonzalo_cirilo@hotmail.com>
2017-11-23 00:05:07 -0500

    Board displaying

---
+++ ttt.go
@@ -0,0 +1,125 @@
+package main
+
+import "fmt"
+
+func drawTab(tab []byte) {
+	fmt.Println("+---+---+---+---+---+---+---+---+")
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("| %c ", tab[i*8 + j])
+		}
+		fmt.Println("|\n+---+---+---+---+---+---+---+---+")
+	}
+}
+
+func scanJugada(tab []byte, p byte) {
+	var i, j byte
+	valid := false
+	for !valid {
+		fmt.Printf("Ficha elegida %c [0-7] [0-7]: ", rune(p))
+		fmt.Scanf("%d %d\n", &i, &j)
+		idx := i + j * 8
+		if i >= 0 && i < 8 && j >= 0 && j < 8 && tab[idx] == p {
+			tab[idx] = 0
+			valid = true
+		} else {
+			fmt.Println(" --- No hay ficha permitida ---")
+		}
+	}
+	valid2 := false
+	for !valid2  {
+		fmt.Printf("Nueva posicion %c [0-7] [0-7]: ", rune(p))
+		fmt.Scanf("%d %d\n", &i, &j)
+		idx := i  + j * 8
+		if i >= 0 && i < 8 && j >= 0 && j < 8 && tab[idx] == ' ' {
+			tab[idx] = p
+			valid2 = true
+		} else {
+			fmt.Println(" --- Jugada no permitida ---")
+		}
+	}
+}
+
+func findWinner(tab []byte) byte {
+	/*for i := 0; i < 8; i++ {
+		if (tab[i*8] == tab[i*8+1] && tab[i*8+1] == tab[i*3+2]) ||
+		   (tab[i] == tab[i+3] && tab[i+3] == tab[i+6]) {
+			return tab[i*4]
+		}
+	}
+	if (tab[0] == tab[4] && tab[4] == tab[8]) ||
+	   (tab[2] == tab[4] && tab[4] == tab[6]) {
+	   	return tab[4]
+	}
+	for i := 0; i < 9; i++ {
+		if tab[i] != 0 {
+			return 0;
+		}
+	}
+	return byte('-')*/ // empate
+	return 0
+}
+
+func chooseOpositeToken(tab [] byte) byte {
+	var token byte
+	for i := 0; i < 64; i++ {
+		if tab[i] != 0 {
+			token = tab[i]
+			break
+		}
+	}
+	if token == 111 {
+		return 120
+	} else if token == 120 {
+		return 111
+	}
+	return 0
+}
+
+func pickToken() byte {
+	p := '-'
+	for p != 'o' && p != 'x' {
+		fmt.Println("Seleccione ficha [x,o]: ")
+		fmt.Scanf("%c\n", &p)
+		if p != 'o' && p != 'x' {
+			fmt.Println(" --- Ficha no permitida ---")
+		}
+	}
+	return byte(p)
+}
+
+func getMsg(buff []byte) byte {
+	return buff[0]
+}
+func getSessId(buff []byte) byte {
+	return buff[1]
+}
+func getPlayerId(buff []byte) byte {
+	return buff[2]
+}
+func getTab(buff []byte) []byte {
+	return buff[3:]
+}
+func setMsg(buff []byte, msg byte) {
+	buff[0] = msg
+}
+func setSessId(buff []byte, sid byte) {
+	buff[1] = sid
+}
+func setPlayerId(buff []byte, pid byte) {
+	buff[2] = pid
+}
+func setTab(buff []byte, tab []byte) {
+	for i, e := range tab {
+		buff[i + 3] = e
+	}
+}
+
+const (
+	NEW = byte(0) // Mensajes del cliente
+	UPDATE = byte(1)
+	PLAY = byte(2)
+
+	WAIT = byte(3)  // Mensajes del server
+	TURN = byte(4)
+)
---
+++ tttserv.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"bufio"
+)
+
+type Sess struct {
+	turn byte
+	play bool
+	tab []byte
+}
+
+var currentSessId byte
+var sessions map[byte]*Sess
+
+func sesion(conn net.Conn, chFirstPlayer chan bool) {
+	defer conn.Close()
+	r := bufio.NewReader(conn)
+	w := bufio.NewWriter(conn)
+	buff := make([]byte,67)
+	buff2 :=[]byte{/*' ',' ',' ',*/
+		'x','x','x','x','x','x','x','x',
+		'x','x','x','x','x','x','x','x',
+		' ',' ',' ',' ',' ',' ',' ',' ',
+		' ',' ',' ',' ',' ',' ',' ',' ',
+		' ',' ',' ',' ',' ',' ',' ',' ',
+		' ',' ',' ',' ',' ',' ',' ',' ',
+		'o','o','o','o','o','o','o','o',
+		'o','o','o','o','o','o','o','o'}
+		//make([]byte, 67)
+	/*for i:=0; i<15; i++{
+		buff[i] = 120
+	}*/
+	setTab(buff,buff2)
+	r.Read(buff)
+	msg := getMsg(buff)
+	sid := getSessId(buff)
+	pid := getPlayerId(buff)
+	tab := getTab(buff)
+	if msg == NEW {
+		if <- chFirstPlayer {
+			sessions[currentSessId] = &Sess{}
+			setSessId(buff, currentSessId)
+			setPlayerId(buff, 0)
+			setMsg(buff, WAIT)
+			setTab(buff,buff2)
+			fmt.Printf("Jugador 1 sesión %d conectado.\n", currentSessId)
+			chFirstPlayer <- false
+		} else {
+			setSessId(buff, currentSessId)
+			setPlayerId(buff, 1)
+			setMsg(buff, TURN)
+			setTab(buff,buff2)
+			sessions[currentSessId].turn = 1
+			fmt.Printf("Jugador 2 sesión %d conectado.\n", currentSessId)
+			currentSessId++
+			chFirstPlayer <- true
+		}
+	} else if msg == PLAY {
+		sessions[sid].tab = tab
+		sessions[sid].play = true
+		setMsg(buff, WAIT)
+	} else if msg == UPDATE {
+		if sessions[sid].turn != pid && sessions[sid].play {
+			setTab(buff, sessions[sid].tab)
+			setMsg(buff, TURN)
+			sessions[sid].play = false
+			sessions[sid].turn = (sessions[sid].turn + 1) % 2
+		} else {
+			setMsg(buff, WAIT)
+		}
+	}
+	w.Write(buff)
+	w.Flush()
+}
+
+func main() {
+	chFirstPlayer := make(chan bool, 1)
+	chFirstPlayer <- true
+	sessions = make(map[byte]*Sess)
+	lstnr, _ := net.Listen("tcp", "localhost:8080")
+	defer lstnr.Close()
+	for {
+		conn, _ := lstnr.Accept()
+		go sesion(conn, chFirstPlayer)
+	}
+}
